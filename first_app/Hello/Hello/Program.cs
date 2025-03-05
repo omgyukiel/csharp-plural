dc58f9e79fabb7754570766836c7c4fd9bc66b69
yukiel <kennylevu1@gmail.com>
2025-03-05 08:37:34 -0800

    feat: add funny virus text

--- first_app/Hello/Hello/Program.cs
+++ first_app/Hello/Hello/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
 Console.WriteLine("Please enter your name: ");
-
 string name = Console.ReadLine();
-Console.WriteLine("Hello " + name);
+Console.WriteLine();
+Console.WriteLine("Hello " + name + ", enjoy your free virus! Your computer has been infected by a free virus, any attempt to remove this virus will delete your entire fucking system. ");
+Console.WriteLine();
+Console.WriteLine("Press Enter to close the terminal; Ciao!");
+Console.ReadLine();
