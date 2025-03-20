d1b15a8a05515a99d6b4e9961165682021a79896
yukiel <kennylevu1@gmail.com>
2025-03-20 09:18:43 -0700

    createing classes and objects in c#

---
+++ PieShop/BethanysPieShopHRM/BethanysPieShopHRM/CodeFile1.cs
@@ -0,0 +1,16 @@
+﻿using BethanysPieShopHRM;
+
+Console.WriteLine("Creating an employee");
+Console.WriteLine("--------------------\n");
+
+Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25);
+
+bethany.DisplayEmployeeDetails();
+
+bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork();
+
+double receivedWageBethany = bethany.ReceiveWage(true);
+Console.WriteLine($"Wage paid (message form Program): {receivedWageBethany}");
